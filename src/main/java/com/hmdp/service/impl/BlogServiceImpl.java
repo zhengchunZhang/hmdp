bced45e0ff8bde113125e1e70ed4afb51bbb18a3
zhangzhengchun <1437237338@qq.com>
2023-04-22 21:09:16 +0800

    p81要实现按时间顺序点赞排行榜 1.使用的set不能满足排序的需求，因此使用sortedSet数据接口来存储，score值为时间 2.要实现排行榜，新增了queryBlogLikes接口 使用了zrange 和 in (5,1) order by field(id , 5 ,1)

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,6 +1,8 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
 import com.hmdp.dto.UserDTO;
@@ -16,7 +18,12 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
 
 /**
  * <p>
@@ -74,11 +81,16 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 
     private void isBlogLiked(Blog blog) {
         //1.获取登录用户
-        Long userId = UserHolder.getUser().getId();
+        UserDTO user = UserHolder.getUser();
+        if(user == null) {
+            //用户未登录 无法查询是否点赞
+            return;
+        }
+        Long userId = user.getId();
         //2.判断当前登录用户是否已经点赞
-        String key = "blog:liked:" + blog.getId();
-        Boolean isMember = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
-        blog.setIsLike(BooleanUtil.isTrue(isMember));
+        String key = BLOG_LIKED_KEY + blog.getId();
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        blog.setIsLike(BooleanUtil.isTrue(score != null));
     }
 
     @Override
@@ -86,15 +98,15 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         //1.获取登录用户
         Long userId = UserHolder.getUser().getId();
         //2.判断当前登录用户是否已经点赞
-        String key = "blog:liked:" + id;
-        Boolean isMember = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
-        if(BooleanUtil.isFalse(isMember)) {
+        String key = BLOG_LIKED_KEY + id;
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        if(score == null) {
             //3.如果未点赞 可以点赞
             //3.1数据库点赞数+1
             boolean isSuccess = update().setSql("liked = liked + 1").eq("id", id).update();
             //3.2保存用户到redis的set集合
             if(isSuccess) {
-                stringRedisTemplate.opsForSet().add(key, userId.toString());
+                stringRedisTemplate.opsForZSet().add(key, userId.toString(),System.currentTimeMillis());
             }
         }else{
             //4.如果已点赞 取消点赞
@@ -102,10 +114,27 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
             boolean isSuccess = update().setSql("liked = liked - 1").eq("id", id).update();
             //4.2 把用户从Redis的set集合移除
             if(isSuccess) {
-                stringRedisTemplate.opsForSet().remove(key,userId.toString());
+                stringRedisTemplate.opsForZSet().remove(key,userId.toString());
             }
 
         }
         return Result.ok();
+    }
+
+    @Override
+    public Result queryBlogLikes(Long id) {
+        String key = BLOG_LIKED_KEY + id;
+        //1.查询top5的点赞用户 zrange key 0 4
+        Set<String> top5 = stringRedisTemplate.opsForZSet().range(key, 0, 4);
+        if(top5 == null || top5.isEmpty()) {
+            return Result.ok(Collections.emptyList());
+        }
+        //2.解析出其中的用户id
+        List<Long> ids = top5.stream().map(Long::valueOf).collect(Collectors.toList());
+        String idStr = StrUtil.join(",",ids);
+        //3.根据用户id查询用户 where id in (5,1) order by field(id , 5 ,1) 根据in(5,1)的顺序查询出来必须要加field,且二者顺序一致
+        List<UserDTO> UserDTOS = userService.query().in("id",ids).last("ORDER BY FIELD (id,"+idStr+")").list().stream().map(user -> BeanUtil.copyProperties(user,UserDTO.class)).collect(Collectors.toList());
+        //4.返回
+        return Result.ok(UserDTOS);
     }
 }

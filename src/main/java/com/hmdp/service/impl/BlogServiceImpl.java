5662e0bf626d1e7bb6b3c45f02d1527941b7b27d
zhangzhengchun <1437237338@qq.com>
2023-04-30 12:40:35 +0800

    p86 p87 滚动分页需要将上一次查询的结果作为下一次的输入。lastId：上一次查询的最小时间戳 offset：上一次查询的最小时间戳元素的个数

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.ScrollResult;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
 import com.hmdp.entity.Follow;
@@ -17,15 +18,18 @@ import com.hmdp.service.IUserService;
 import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
+import static com.hmdp.utils.RedisConstants.FEED_KEY;
 
 /**
  * <p>
@@ -164,5 +168,50 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         }
         // 返回id
         return Result.ok(blog.getId());
+    }
+
+    @Override
+    public Result queryBlogOfFollow(Long max, Integer offset) {
+        //1.获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        
+        //2.查询收件箱
+        String key = FEED_KEY + userId;
+        Set<ZSetOperations.TypedTuple<String>> typedTuples = stringRedisTemplate.opsForZSet().reverseRangeByScoreWithScores(key, 0, max, offset, 2);
+        if(typedTuples == null || typedTuples.isEmpty()) {
+            return Result.ok();
+        }
+        //3.解析数据
+        List<Long> ids = new ArrayList<>(typedTuples.size());
+        long minTime = 0;
+        int os = 1;
+        for(ZSetOperations.TypedTuple<String> tuple : typedTuples){
+            ids.add(Long.valueOf(tuple.getValue()));
+            long time = tuple.getScore().longValue();
+            if(time == minTime) {
+                ////如果是最小的时间戳，个数+1
+                os++;
+            }else {
+                //如果不是最小的时间戳，将个数重置为1
+                minTime = time;
+                os = 1;
+            }
+        }
+        //4.根据id查询blog
+//        List<Blog> blogs = listByIds(ids);//基于mysql的in实现的，in不能保证返回前端的list是有序的
+        String idStr = StrUtil.join(",", ids);
+        List<Blog> blogs = query().in("id", ids).last("ORDER BY FIELD(id," + idStr + ")").list();
+        for(Blog blog : blogs) {
+            this.queryBlogUser(blog);
+            this.isBlogLiked(blog);
+        }
+
+
+        //5.封装并返回
+        ScrollResult r = new ScrollResult();
+        r.setOffset(os);
+        r.setList(blogs);
+        r.setMinTime(minTime);
+        return Result.ok(r);
     }
 }
